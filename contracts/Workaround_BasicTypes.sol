7945fac9de757e86c14da069f09f81b9edb18585
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-04-01 01:14:25 +0200

    Added Previous version workaround for math operations

--- contracts/Workaround_BasicTypes.sol
+++ contracts/Workaround_BasicTypes.sol
@@ -13,6 +13,22 @@ contract BasicTypes{
 
     int public myInt = -10; //-2**128 and +2**128-1
 
+    //todo strings
+    string public greeting = "Hello World";
+
+    //Return the bytes representation of the string above and this is the preferred usage instead of the Strings
+    bytes public myBytesGreeting = "Hello World";
+
+    function setMyString (string memory _myGreeting) public {
+        greeting = _myGreeting;
+    }
+
+    //Public view function is a reading function and it must return a value
+    function compareStrings(string memory _myGreeting2) public  view returns(bool){
+        return keccak256(abi.encodePacked(greeting)) == keccak256(abi.encodePacked(_myGreeting2));
+
+    }
+
     
     function setMyUint(uint _myUint) public {
         myUint = _myUint;
